e6dc02ff59bd38f770f362fac5459cc5a6193682
Christophe Rodriguez <chris@iperlon.fom>
2015-04-27 21:27:16 +0200

    Initial commit

---
+++ angular-hash.js
@@ -0,0 +1,9 @@
+var angularHash = angular.module('angularHash', []);
+angularHash.factory('hashService', function() {
+    var shinyNewServiceInstance;
+
+
+
+    // factory function body that constructs shinyNewServiceInstance
+    return shinyNewServiceInstance;
+});
